55d93a101fca2e79948aedb1a312af9cb6e192a7
lisanaples <lisanaplesrealtor@gmail.com>
2026-09-23 12:15:21 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "lead-router-v14";
+const CACHE_NAME = "lead-router-v15";
 const APP_SHELL = [
   "./",
   "index.html",
-  "styles.css?v=14",
-  "app.js?v=14",
+  "styles.css?v=15",
+  "app.js?v=15",
   "assets/lead-router-icon.svg",
   "manifest.webmanifest"
 ];
